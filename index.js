abce734f6e8811a19c14bd6d90debb7c37017f20
Laura <laurao148@gmail.com>
2018-08-17 02:24:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function(drivers, firstOrLastTwoDrivers {
+const selectDifferentDrivers = function(drivers, firstOrLastTwoDrivers) {
   return firstOrLastTwoDrivers(drivers);
 };
